0139b644ede49900e0758b2ed4fc677423aa98a1
birdy <gob@live.ru>
2018-02-19 23:11:05 +0300

    * names and a bit of renaming

--- hw1.7/index.py
+++ hw1.7/index.py
@@ -1,58 +1,67 @@
 class Animal:
-    title = 'животное'
+    type = 'животное'
+    name = ''
     can_fly = False
     sound = ''
 
-    def __init__(self):
-        print('Создано: %s' % self.title)
+    def __init__(self, name='(забыли назвать)'):
+        self.name = name
+        print('Создано: %s' % self.__str__())
 
     def make_sound(self):
         if not self.sound:
             return
-        print('%s издает звук "%s"' % (self.title, self.sound))
+        print('%s издает звук "%s"' % (self.name, self.sound))
+
+    def __str__(self):
+        return '%s: %s' % (self.type, self.name)
 
 
-class FlyingAnimal(Animal):
-    title = 'летающее животное'
+class Mammal(Animal):
+    type = 'млекопитающее'
+
+
+class Bird(Animal):
+    type = 'птица'
     can_fly = True
 
 
 class Cow(Animal):
-    title = 'корова'
+    type = 'корова'
     sound = 'муу'
 
 
 class Goat(Animal):
-    title = 'коза'
+    type = 'коза'
     sound = 'мее'
 
 
 class Sheep(Animal):
-    title = 'овца'
+    type = 'овца'
     sound = 'бее'
 
 
 class Pig(Animal):
-    title = 'свинья'
+    type = 'свинья'
     sound = 'хрю'
 
 
-class Duck(FlyingAnimal):
-    title = 'утка'
+class Duck(Bird):
+    type = 'утка'
     sound = 'кря'
 
 
-class Hen(FlyingAnimal):
-    title = 'курица'
+class Hen(Bird):
+    type = 'курица'
     sound = 'куд-кудах'
 
 
-class Goose(FlyingAnimal):
-    title = 'гусь'
+class Goose(Bird):
+    type = 'гусь'
     sound = 'га... га'
 
 
-animals = [Pig(), Sheep(), FlyingAnimal(), Hen(), Goose()]
+animals = [Pig('Марфа'), Cow('Зорька'),  Cow('Манька'), Bird(), Hen(), Goose('Потап')]
 
 for animal in animals:
     animal.make_sound()
