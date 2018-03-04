204e7cabde9344419eacddd9dc7d5f5f0a559cf2
birdy <gob@live.ru>
2018-03-04 12:08:57 +0300

    * homework 2.4 (search in files)

---
+++ hw2.4/find_procedure.py
@@ -0,0 +1,75 @@
+# Задание
+# мне нужно отыскать файл среди десятков других
+# я знаю некоторые части этого файла (на память или из другого источника)
+# я ищу только среди .sql файлов
+# 1. программа ожидает строку, которую будет искать (input())
+# после того, как строка введена, программа ищет её во всех файлах
+# выводит список найденных файлов построчно
+# выводит количество найденных файлов
+# 2. снова ожидает ввод
+# поиск происходит только среди найденных на этапе 1
+# 3. снова ожидает ввод
+# ...
+# Выход из программы программировать не нужно.
+# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
+
+# Пример на настоящих данных
+
+# python3 find_procedure.py
+# Введите строку: INSERT
+# ... большой список файлов ...
+# Всего: 301
+# Введите строку: APPLICATION_SETUP
+# ... большой список файлов ...
+# Всего: 26
+# Введите строку: A400M
+# ... большой список файлов ...
+# Всего: 17
+# Введите строку: 0.0
+# Migrations/000_PSE_Application_setup.sql
+# Migrations/100_1-32_PSE_Application_setup.sql
+# Всего: 2
+# Введите строку: 2.0
+# Migrations/000_PSE_Application_setup.sql
+# Всего: 1
+
+# не забываем организовывать собственный код в функции
+
+import os
+
+migrations = 'Migrations'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+migrations_dir = os.path.join(current_dir, 'Migrations')
+files = os.listdir(migrations_dir)
+
+
+def detect_charset(file_path):
+    import chardet
+    file = open(file_path, 'rb+')
+    data = file.readline()
+    return chardet.detect(data)['encoding']
+
+
+def find_in_file(string, filename):
+    file_path = os.path.join(migrations_dir, filename)
+    try:
+        for line in open(file_path, 'r+', encoding=detect_charset(file_path)).readlines():
+            found = string in line.lower()
+            if found:
+                return True
+    except Exception as e:
+        pass
+    return False
+
+
+if __name__ == '__main__':
+    while len(files) > 1:
+        search_string = input('Введите строку: ').lower()
+        files = [file for file in files if find_in_file(search_string, file)]
+        files_count = len(files)
+        if files_count > 10:
+            print('... большой список файлов ...')
+        else:
+            for file in files:
+                print('Migrations/%s' % file)
+        print('Всего %d' % files_count)
